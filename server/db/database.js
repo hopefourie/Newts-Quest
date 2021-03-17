eeee0159d92a2e3f3ba9eef7e26725c52ea2b5b7
HopeFourie <hopefourie@gmail.com>
2021-03-17 16:48:53 -0400

    fix: Sequelize config dialectOptions

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,36 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/newts-quest',
-  {
+const pkg = require('../../package.json');
+
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+
+let config;
+
+if (process.env.DATABASE_URL) {
+  config = {
     logging: false,
-  }
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+  };
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 );
 
 module.exports = db;
+
+if (process.env.NODE_ENV === 'test') {
+  after('close database connection', () => db.close());
+}
